6a3fe2c07807090fc4fc31bc83dc6199aa5fb805
Fernando Koss <fermkoss@gmail.com>
2025-04-19 21:11:29 -0300

    agregado del componente Frases

---
+++ src/components/frases/Frases.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./frases.css"
+import a from "../../../public/images-frases/coleman.jpg"
+const Frases = () => {
+
+
+    const [numFrase, setNumFrase] = useState(0);
+
+    const listaFrases = [
+        {
+            img: "../../../public/images-frases/arnold.avif",
+            frase: `"La fuerza no viene de ganar. Tus luchas desarrollan tus fortalezas. Cuando atraviesas dificultades y decides no rendirte, esa es la fuerza."`,
+            autor: "Arnold Schwarzenegger"
+        },
+        {
+            img: "../../../public/images-frases/stallone.jpg",
+            frase: `"Aguantar una ronda más cuando crees que no puedes, eso es lo que marca la diferencia en tu vida."`,
+            autor: "Sylvester Stallone"
+        },
+        {
+            img: "../../../public/images-frases/coleman.jpg",
+            frase: `"Todo el mundo quiere ser fisicoculturista, ¡pero nadie quiere levantar pesos!"`,
+            autor: "Ronnie Coleman"
+        }
+    ]
+
+
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setNumFrase((prev) => (prev == listaFrases.length - 1 ? 0 : prev + 1));
+
+    }, 5000);
+
+    return () => clearInterval(interval);
+}, []);
+/*
+setInterval(() => {
+    setNumFrase(numFrase == listaFrases.length - 1 ? 0 : numFrase + 1);
+}, 5000);*/
+
+
+    return (
+        <div className="div-frases">
+            <div className="div-img-frases">
+                <img src={listaFrases[numFrase].img} alt="" />
+            </div>
+            <div className="div-frase-frases">
+                <p>{listaFrases[numFrase].frase}</p>
+                <h2>{listaFrases[numFrase].autor}</h2>
+            </div>
+        </div>
+    );
+}
+export default Frases;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import Frases from "../frases/Frases";
 
 const Layout = () => {
   return (
@@ -10,7 +11,9 @@ const Layout = () => {
         <Outlet /> {/* Esto renderizará las páginas */}
       </main>
       {/* Aquí podrías agregar un Footer si lo necesitas */}
+      <Frases/>
       <Footer/>
+      
     </div>
   );
 };
